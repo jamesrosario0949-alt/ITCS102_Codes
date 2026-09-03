5ddb1beca098703588e9b7564e25616cbab6520f
James D. Rosario <jamesrosario0949@gmail.com>
2026-09-03 11:20:49 +0800

    Update activity6.py

--- activity6.py
+++ activity6.py
@@ -7,10 +7,10 @@ print("so you need to gain", sum, "out of 70% to achieve your prefer grades")
 
 z = int(input("What grades do you prefer?:"))
 
-round = z-60
+round = 60-z
 print("so you need to gain", round, "out of 60% to achieve your prefer grades")
 
 x = int(input("What grades do you prefer?:"))
 
-point = y-50
+point = 50-x
 print("so you need to gain", point, "out of 50% to achieve your prefer grades")
