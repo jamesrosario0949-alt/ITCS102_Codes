8e7c0490c39ae70000ed2b9bf318e75550c19286
James D. Rosario <jamesrosario0949@gmail.com>
2026-09-03 11:30:11 +0800

    IT1A_ROSARIO

--- codechallenge_2.py
+++ codechallenge_2.py
@@ -1,4 +1,4 @@
-money = 1540
+money = 5540
 a = 1000
 b = 500
 c = 200
@@ -7,4 +7,19 @@ e = 50
 f = 20
 h = 1
 
-print("1000 -", money // a, "\n500 -", money // b, "\n200 -", money // c, "\n100 -", money // d, "\n50 -", money // e, "\n20 -", money // f, "\n1 -", money // h)
+GOT = money // 1000
+log = money % 1000
+hog = log // 500
+lig = log % 500
+pog = lig // 200
+pig = lig % 200
+kid = pig // 100
+kids = pig % 100
+mod = kids // 50
+mods = kids % 50
+dog = mods // 20
+dogs = mods % 20
+hog = dogs // 1
+hogs = dogs % 1
+
+print("1000 -", GOT, " \n 500-", hog, " \n 200 -" pog, "\n 100 -" kid, "\n 50-", "\n 20-" 
