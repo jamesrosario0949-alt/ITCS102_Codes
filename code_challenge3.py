4a7f8b67e6b1fd22718b91e7780d0bdc29fe017e
James D. Rosario <jamesrosario0949@gmail.com>
2026-09-10 19:58:13 +0800

    IT1A_ROSARIO

--- code_challenge3.py
+++ code_challenge3.py
@@ -1,14 +1,35 @@
-base_cost = float(input("Enter the weight: "))
-base_cost2 = float(input("Enter the distance: "))
+name = input("What is your name:  ")
+Type_item = input("What is the item you wanted to buy:   ")
+is_frag = bool(input("input the fragile yes/no:    "))
+w = float(input("input the weight kg:   "))
+d = float(input("input the distance  km:  "))
+is_express = bool(input("is it express yes/no;   "))
+is_international = bool(input("is it international yes/no? :  "))
+base_cost = (w * 2.50) + (d * 0.15)
+
+print("-------> Summary of the item <--------")    
+print("sender:", name)
+print("Item:", Type_item)
+print("Is it Fragile;", is_frag)
+print("weight:", w)
+print("distance", d)
 
 
-if base_cost >= 30 or base_cost2 >= 1000:
-    print("Oversized, FALSE, FALSE")
-elif base_cost * 1.40 + 30 == base_cost2 * 1.40 + 30:
-    print("International Express")
-elif base_cost >= 20 and base_cost2 * 1.20 + 25:
-    print("Express or Heavy International")
-elif base_cost >= 2 and base_cost2 <= 100 and base_cost <= 1.9 and base_cost2 <= 100:
-    print("Free shipping, FALSE, FALSE")
+if w <= 2 and d <= 100 and not is_express and not is_international:
+    print("Free shipping")
+    print("the total cost; 0.00 $ \n")
+elif is_express and is_international:
+    print("Shipping rate international express" )
+    print("the total cost:", (base_cost * 1.40) + 50 )
+elif is_express or (is_international and w > 20):
+    print("shipping rate; Express or Heavy international")
+    print("the total cost:", (base_cost * 1.20)+ 25 )
+elif w > 30 or d > 1000: 
+    print("shipping rate: Oversized")
+    print("the total cost:", base_cost + 30 )
+elif is_frag == True:
+    print("the frag is true \n")
 else:
-    print("Standard Shipping")
+    print("Shipping rate: Standard")
+    print("the total cost:", base_cost)
+    
