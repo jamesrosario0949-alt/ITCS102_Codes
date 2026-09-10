ee1fa3304d68cc75423bdf2d6f3252e28c104149
James D. Rosario <jamesrosario0949@gmail.com>
2026-09-10 22:50:50 +0800

    Update code_challenge3.py

--- code_challenge3.py
+++ code_challenge3.py
@@ -10,8 +10,8 @@ base_cost = (w * 2.50) + (d * 0.15)
 print("=======> Summary of the item <========")    
 print("sender:", name)
 print("Item:", Type_item)
-print("weight:", w)
-print("distance", d)
+print("weight:", w,"kg")
+print("distance", d,"km")
 if is_express == "True":
     print("express: True")
 else: 
@@ -29,13 +29,13 @@ if w <= 2 and d <= 100 and is_express == "False" and is_international == "False"
     print("Free shipping")
     print("the total cost; 0.00 $ \n")
 elif is_express == "True" and is_international == "False":
-    print("Shipping rate international express" )
+    print("Shipping rate: Expedited Shipping" )
     print("the total cost:", (base_cost * 1.40) + 50 )
 elif is_express or (is_international and w > 20):
-    print("shipping rate; Heavy Size")
+    print("shipping rate; Price-Based")
     print("the total cost:", (base_cost * 1.20)+ 25 )
 elif w > 30 or d > 1000: 
-    print("shipping rate: Oversized")
+    print("shipping rate: Standard Ground Shipping")
     print("the total cost:", base_cost + 30 )
 elif is_frag == True:
     print("the frag is true \n")
